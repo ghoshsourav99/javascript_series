d4ea61cf3b88905e1b4e500ae7ffbeae8a77f011
Sourav Ghosh <146316533+ghoshsourav99@users.noreply.github.com>
2024-11-26 10:23:30 +0000

    add variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "souravghosh8891@gmail.com"
+var accountPassword = "1234"
+accountCity = "kolkata"
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+// accountId = 76478
+accountEmail = "svjheh253@gmil.com"
+accountPassword = "874643"
+accountCity= "jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as newer version
+alert(3+3)  // this is not the right syntax here it gives an error
+
+// this are primitive data types
+
+let age = 18          // number
+let name = "sourav"    // sourav
+let isLoggedIn = false // boolean
+                       // null
+let state;             // undefined
+                        //symbol
+
+  // non primitive data types
+  // object
+
+
+
+  console.log(typeof "sourav");
+  console.log(typeof undefined);  // undefined
+  console.log(typeof null);   // object
+  
+  
+  
